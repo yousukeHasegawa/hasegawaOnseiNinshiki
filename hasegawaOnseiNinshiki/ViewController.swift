48eedff9e72b61a20fec2712dae359a8b7595b30
yousukeHasegawa <hasegawa.yousuke.mn@mitsubishichem-sys.co.jp>
2021-11-08 11:40:48 +0900

    Initial Commit

---
+++ hasegawaOnseiNinshiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hasegawaOnseiNinshiki
+//
+//  Created by Yousuke Hasegawa on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
